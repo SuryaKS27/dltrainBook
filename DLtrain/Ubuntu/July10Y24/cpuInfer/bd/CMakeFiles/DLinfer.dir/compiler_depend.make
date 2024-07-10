38f6249160c2836473e274cd85b00c92d1da240e
jk <58679469+DLinIoTedge@users.noreply.github.com>
2024-07-10 20:56:35 +0530

    Add files via upload

---
+++ DLtrain/Ubuntu/July10Y24/cpuInfer/bd/CMakeFiles/DLinfer.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for DLinfer.
+# This may be replaced when dependencies are built.
---
+++ DLtrain/Ubuntu/July10Y24/cpuTrain/bd/CMakeFiles/DLtrain.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "../bin/DLtrain"
+  "../bin/DLtrain.pdb"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcInfer/MNISTRead.cpp.o"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcInfer/MNISTRead.cpp.o.d"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcInfer/feedforward.cpp.o"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcInfer/feedforward.cpp.o.d"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcInfer/matrix.cpp.o"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcInfer/matrix.cpp.o.d"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcTrain/CNN.cpp.o"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcTrain/CNN.cpp.o.d"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcTrain/main.cpp.o"
+  "CMakeFiles/DLtrain.dir/home/jk/dev/DLtrain/TestJuly10Y24/srcTrain/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DLtrain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
